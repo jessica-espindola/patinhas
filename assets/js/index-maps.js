8d4e9ae91553b119a7c9ad968855c8fb0dc07ba7
Jéssica Espíndola <jessiicav.1@gmail.com>
2024-04-13 12:22:54 -0300

    alterando configuração de consumo de api

--- assets/js/index-maps.js
+++ assets/js/index-maps.js
@@ -1,55 +1,30 @@
-// Note: This example requires that you consent to location sharing when
-// prompted by your browser. If you see the error "The Geolocation service
-// failed.", it means you probably did not give permission for the browser to
-// locate you.
-let map, infoWindow;
+var map = L.map('map').setView([-8.047562, -34.877002], 13); // Define as coordenadas de Recife
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 6,
-  });
-  infoWindow = new google.maps.InfoWindow();
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+}).addTo(map);
 
-  const locationButton = document.createElement("button");
+// Adicione um marcador para a localização do usuário
+navigator.geolocation.getCurrentPosition(function (position) {
+    var userLat = position.coords.latitude;
+    var userLng = position.coords.longitude;
+    L.marker([userLat, userLng]).addTo(map)
+        .bindPopup('Sua Localização Atual')
+        .openPopup();
+});
 
-  locationButton.textContent = "Ir para minha localização";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
+// Adicione marcadores para pontos pré-cadastrados em Recife
+var points = [
+    { name: "Hospital Veterinário Harmonia", latlng: [-8.11769, -34.89378] },
+    { name: "Hospital Veterinário Seres", latlng: [-8.11744, -34.90339] },
+    { name: "Pet Dream Hospital Veterinário", latlng: [-8.11362, -34.89026] }
+];
 
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        },
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
-}
+points.forEach(function (point) {
+    L.marker(point.latlng).addTo(map).bindPopup(point.name);
+});
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation.",
-  );
-  infoWindow.open(map);
-}
-
-window.initMap = initMap;
+// Atualize o tamanho do mapa quando a janela for redimensionada
+window.addEventListener('resize', function () {
+    map.invalidateSize();
+});
